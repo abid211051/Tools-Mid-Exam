a427223a5a9eb09d724355cd024d3be805913ad8
abid211051 <c211051@ugrad.iiuc.ac.bd>
2023-03-29 19:53:47 +0600

    1st coomit

---
+++ DOM/script.js
@@ -0,0 +1,28 @@
+// function changecolor(){
+//     document.querySelector("div").style.backgroundColor = blue;
+// }
+
+function calculate(v, v1, v2){
+    let res;
+    if(v =="triangle"){
+        res = 0.5 * v1.value * v2.value;
+    }
+    else if(v== "rectangle" || v == "Parallelogram"){
+        res = v1.value * v2.value;
+    }
+    document.getElementById('demo').innerHTML += `${v} ${res}cm <br>`;
+}
+
+function randomColor() {
+    let color = [];
+    for (let i = 0; i < 4; i++) {
+      color.push(Math.floor(Math.random() * 256));
+    }
+    return 'rgb(' + color.join(', ') + ')';
+  } 
+  
+function colorchange(val){
+    let x = document.getElementById(val);
+    console.log(x);
+    x.style.backgroundColor = randomColor() ;
+}
