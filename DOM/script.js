d87291566f6c307f8a887c7fba1b0c67de623db1
abid211051 <c211051@ugrad.iiuc.ac.bd>
2023-03-30 03:20:16 +0600

    4th commit

--- DOM/script.js
+++ DOM/script.js
@@ -1,16 +1,19 @@
 
 function newdiv(v, res){
-    console.log("called");
+    // console.log("called");
     var newdiv = document.createElement('div');
     var btn = document.createElement('button');
     var ptag = document.createElement('p');
     newdiv.className= 'newdiv';
-    ptag.innerHTML = `${v} ${res}cm`;
+    ptag.innerHTML = `${v} ${res} cm`;
     btn.onclick = function(event){
         var parentDiv = event.target.parentElement;
         convertToMeter(parentDiv);
     };
-    btn.innerHTML = 'click here';
+    btn.innerHTML = 'Convert To Meter';
+    btn.style.width = '120px';
+    btn.style.height = '30px';
+    btn.style.backgroundColor = "mediumspringgreen"
     newdiv.appendChild(ptag);
     newdiv.appendChild(btn);
     newdiv.style.display = 'flex';
@@ -41,23 +44,23 @@ function convertToMeter(parentDiv){
         newval = parseInt(str);
         newval = newval/100;
     }
-    if(y.includes("triangle")){
-        x.innerHTML = `triangle ${newval} m<sup>2</sup>`;
+    if(y.includes("Triangle")){
+        x.innerHTML = `Triangle ${newval} m<sup>2</sup>`;
     }
-    else if(y.includes("rectangle")){
-        x.innerHTML = `rectangle ${newval} m<sup>2</sup>`;
+    else if(y.includes("Rectangle")){
+        x.innerHTML = `Rectangle ${newval} m<sup>2</sup>`;
     }
     else{
-        x.innerHTML = `parallelogram ${newval} m<sup>2</sup>`;
+        x.innerHTML = `Parallelogram ${newval} m<sup>2</sup>`;
     }
     b.disabled = true;
 }
 function calculate(v, v1, v2) {
     let res;
-    if (v == "triangle") {
+    if (v == "Triangle") {
         res = 0.5 * v1.value * v2.value;
     }
-    else if (v == "rectangle" || v == "parallelogram") {
+    else if (v == "Rectangle" || v == "Parallelogram") {
         res = v1.value * v2.value;
     }
 
