4654d99f758a17c4d11e9736004d4eb49c94fa32
abid211051 <c211051@ugrad.iiuc.ac.bd>
2023-03-30 03:10:53 +0600

    3rd commit

--- DOM/script.js
+++ DOM/script.js
@@ -1,19 +1,70 @@
-// function changecolor(){
-//     document.querySelector("div").style.backgroundColor = blue;
-// }
 
-function calculate(v, v1, v2){
+function newdiv(v, res){
+    console.log("called");
+    var newdiv = document.createElement('div');
+    var btn = document.createElement('button');
+    var ptag = document.createElement('p');
+    newdiv.className= 'newdiv';
+    ptag.innerHTML = `${v} ${res}cm`;
+    btn.onclick = function(event){
+        var parentDiv = event.target.parentElement;
+        convertToMeter(parentDiv);
+    };
+    btn.innerHTML = 'click here';
+    newdiv.appendChild(ptag);
+    newdiv.appendChild(btn);
+    newdiv.style.display = 'flex';
+    newdiv.style.gap = '70px'
+    newdiv.style.marginBottom ='10px';
+    return newdiv;
+}
+function convertToMeter(parentDiv){
+    var x = parentDiv.querySelector('p');
+    var b = parentDiv.querySelector('button');
+    const y = x.innerHTML;
+    let str ="";
+    let f =0, newval;
+    for(let i = 0;i<y.length;i++){
+        if(y[i]>='0' && y[i]<='9'){
+            str+=y[i];
+        }
+        else if(y[i]=='.'){
+            f=1;
+            str+=y[i];
+        }
+    }
+    if(f){
+        newval = parseFloat(str);
+        newval = newval /100;
+    }
+    else{
+        newval = parseInt(str);
+        newval = newval/100;
+    }
+    if(y.includes("triangle")){
+        x.innerHTML = `triangle ${newval} m<sup>2</sup>`;
+    }
+    else if(y.includes("rectangle")){
+        x.innerHTML = `rectangle ${newval} m<sup>2</sup>`;
+    }
+    else{
+        x.innerHTML = `parallelogram ${newval} m<sup>2</sup>`;
+    }
+    b.disabled = true;
+}
+function calculate(v, v1, v2) {
     let res;
-    if(v =="triangle"){
+    if (v == "triangle") {
         res = 0.5 * v1.value * v2.value;
     }
-    else if(v== "rectangle" || v == "Parallelogram"){
+    else if (v == "rectangle" || v == "parallelogram") {
         res = v1.value * v2.value;
     }
-    document.getElementById('demo').innerHTML += `${v} ${res}cm <br>`;
+
+    document.getElementById('output').appendChild(newdiv(v,res));
 }
-  
-function colorchange(val){
+
+function colorchange(val) {
     let x = document.getElementById(val);
-    x.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})` ;
+    x.style.backgroundColor = `rgb(${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)},${Math.floor(Math.random() * 256)})`;
 }
